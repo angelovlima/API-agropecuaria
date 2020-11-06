ae53a3015a1427c2fa3a64ff2fbc8d2b80fc120a
angelovlima <angelovictor2001@hotmail.com>
2020-11-06 20:08:13 -0300

    teste

--- src/main/java/br/com/fatec/agropecuaria/App.java
+++ src/main/java/br/com/fatec/agropecuaria/App.java
@@ -16,7 +16,5 @@ public class App
         System.out.println("Peso: " + boizinho.getPeso());
         System.out.println("Tomou Vacina: " + boizinho.getTomouVacina() != null ? "Seu boizinho é vacinado e feliz" : "Falta vacina");
         //Tábatha testando 
-        //Teste do Fork jkhjhkjhgjg dxfgg
-        //Teste do Fork
     }
 }
