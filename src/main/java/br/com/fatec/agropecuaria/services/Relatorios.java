7029e8eb5488bbafb6ecb7f87079d86e56df3859
angelovlima <angelovictor2001@hotmail.com>
2020-11-06 23:16:25 -0300

    Relatorios

---
+++ src/main/java/br/com/fatec/agropecuaria/services/Relatorios.java
@@ -0,0 +1,29 @@
+package br.com.fatec.agropecuaria.services;
+
+public class Relatorios {
+
+	public void quantidadeTotalAnimaisCadastrados() {
+		System.out.println("Quantidade total de animais cadastrados");
+	}
+	
+	public void precoDoRebanho() {
+		System.out.println("Preco total rebanho");
+	}
+	
+	public void pesoDoRebanhoQuiloArroba() {
+		System.out.println("Peso total quilo");
+		System.out.println("Peso total arroba");
+	}
+	
+	public void quantidadeFemeaMacho() {
+		System.out.println("Quantidade Femeas");
+		System.out.println("Quantidade Machos");
+	}
+	
+	public void dadosVacina() {
+		System.out.println("Porcentagem do rebanho vacinada");
+		System.out.println("Quantos faltam vacinar");
+	}
+	
+	
+}
