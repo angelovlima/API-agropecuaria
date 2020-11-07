191a6c35572a7c440e6f3d4e8b979b54665a028d
Miho <mihojp123@gmail.com>
2020-11-06 22:41:29 -0300

    fix vacina

---
+++ src/main/java/br/com/fatec/agropecuario/model/vacina/Carterinha.java
@@ -0,0 +1,14 @@
+package br.com.fatec.agropecuario.model.vacina;
+
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Carterinha {
+		private ArrayList<Vacina> carterinha;
+		
+}
