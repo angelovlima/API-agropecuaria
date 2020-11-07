96ca6519d566d0a1509e3b690444381d1da36f98
Tábatha Fróes <tabathafroes@gmail.com>
2020-11-06 22:01:52 -0300

    Ajustes

--- src/main/java/br/com/fatec/agropecuaria/model/animais/Suino.java
+++ src/main/java/br/com/fatec/agropecuaria/model/animais/Suino.java
@@ -1,7 +1,13 @@
 package br.com.fatec.agropecuaria.model.animais;
 
-public class Suino extends Animal{
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Suino extends Animal{
+	
 	public Suino(Double id, String registroUnico, Double peso, String raca, String genero, String dataNascimento) {
 		super(id, registroUnico, peso, raca, genero, dataNascimento);
 		
